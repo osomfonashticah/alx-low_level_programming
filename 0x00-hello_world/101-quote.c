98b1d6c957902d9d05ac0b4a350b88b378ed4fe9
Joseph Darko <osomfonashticah@gmail.com>
2023-02-16 15:19:12 -0800

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry function
+ * Return: Always 1
+ */
+
 int main(void)
-{
+{    
 	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	char a;
-	int i;
+	int a;
 	long int b;
 	long long int c;
+	char d;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
+};
