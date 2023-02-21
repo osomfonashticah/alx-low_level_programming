83de3f77da59db14e778f6bc8f704f324c587f99
Joseph Darko <osomfonashticah@gmail.com>
2023-02-21 10:05:06 -0800

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,13 +10,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-void print_alphabet(void)
-{
-	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,10 @@
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
