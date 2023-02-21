aae4d93f100c8438c82e69e8ba60d9cc7dec54e8
Joseph Darko <osomfonashticah@gmail.com>
2023-02-21 14:58:13 -0800

    changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add - prints two integer values added together.
+ *Return: Always 0.
+ *@a: a variable of type integer.
+ *@b:a variable of type integer.
+ */
+int add(int a, int b)
+{
+	printf("%d\n", a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_last_digit(int c);
+int add(int a, int b);
 
 
 
