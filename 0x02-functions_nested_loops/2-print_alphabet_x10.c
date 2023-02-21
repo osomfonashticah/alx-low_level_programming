9b0e9c1deb3f90048ae9349707b906616b71b350
Joseph Darko <osomfonashticah@gmail.com>
2023-02-21 11:04:38 -0800

    changes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alhapbet_x10 - a function that prints 10 times the alphabet.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		char ch;
+
+		for (ch = 'a' ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	putchar('\n');
+}
