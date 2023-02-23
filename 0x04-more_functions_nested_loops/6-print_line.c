8be35f8e75e109bebd5189e219c21c59cd4be1dd
Joseph Darko <osomfonashticah@gmail.com>
2023-02-23 11:57:13 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - prints lines n times
+ * @n:number of times the lines are to be printed
+ * Returns:Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	_putchar('\n');
+}
