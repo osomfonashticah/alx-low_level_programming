0bc4b8fad182e3f565aff05efd0ad3689788dd0b
Joseph Darko <osomfonashticah@gmail.com>
2023-02-23 12:04:56 -0800

    changes

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,11 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		if (n < 0)
-			_putchar('\n');
-		else
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
 			_putchar('_');
+
+	}
 	_putchar('\n');
 }
