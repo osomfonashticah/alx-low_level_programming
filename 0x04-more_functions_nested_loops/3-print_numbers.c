b618254d5c8b58b7fc849a2d39244e19b068a1cd
Joseph Darko <osomfonashticah@gmail.com>
2023-02-23 10:09:07 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Returns: Always 0
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
