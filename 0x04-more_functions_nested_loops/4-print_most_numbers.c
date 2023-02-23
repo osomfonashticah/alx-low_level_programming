007bff96c598836d727d554c7a303672381c1c4b
Joseph Darko <osomfonashticah@gmail.com>
2023-02-23 10:29:12 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9
+ * excepts 4 and 2
+ * Returns:Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar((i % 10) + '0');
+		if (i == 2 || i == 4)
+			continue;
+		i++;
+	}
+}
