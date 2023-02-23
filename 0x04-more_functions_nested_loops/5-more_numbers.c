e589a89c464bd64dbaddeba0fda8f6a148797ad3
Joseph Darko <osomfonashticah@gmail.com>
2023-02-23 11:20:44 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ * Return:Always 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+		for (j = 0; j < 15; j++)
+		{
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	_putchar('\n');
+
+
+}
