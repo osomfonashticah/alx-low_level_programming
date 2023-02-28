0c30aab06d785d7081a9929b857e3a485c9b624b
Joseph Darko <osomfonashticah@gmail.com>
2023-02-28 12:39:47 -0800

    changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,11 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+int i = 0;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+	while (s[i])
+		i++;
+	while (i--)
 		_putchar(s[i]);
-
 	_putchar('\n');
 }
