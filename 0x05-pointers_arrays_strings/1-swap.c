3facc574bfd62cac14c411b83462b58645d53e71
Joseph Darko <osomfonashticah@gmail.com>
2023-02-28 10:12:45 -0800

    changes

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * swap_int - swaps 2 variables
  * @a:variable to be swappped
  * @b:variable to be swapped
+ * return : always 0
  */
 
 void swap_int(int *a, int *b)
@@ -12,5 +14,5 @@ void swap_int(int *a, int *b)
 
 	tmp = *a;
 	*a = *b;
-	tmp = *b;
+	*b = tmp;
 }
