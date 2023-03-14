e90f0bbb45e37a0fb434eaf2c203672d16af4e82
Joseph Darko <osomfonashticah@gmail.com>
2023-03-14 16:17:47 +0000

    changes

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - function that multiplies two integers
+ * @argc: argument count
+ * @argv:argument array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, result, i;
+
+	if (!argv[i])
+		printf("Error");
+	result = a * b;
+	printf("%d\n", result);
+	return (0);
+}
