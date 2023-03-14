4cc4cbd80d5de1c9c3c2757ec33245d8aeb5e10e
Joseph Darko <osomfonashticah@gmail.com>
2023-03-14 01:46:23 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - function to print it name
+ * @argc:counts number of inputs
+ * @argv:shows commandline inputs
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
