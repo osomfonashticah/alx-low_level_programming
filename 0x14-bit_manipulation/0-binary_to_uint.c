f2d903fc52aef1039bfbd43b6245c55a3439dcb9
Joseph Darko <osomfonashticah@gmail.com>
2023-04-07 19:21:17 +0000

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,21 +6,24 @@
  * Return:converted decimal
  */
 
-unsigned int binary_to_uint(const char *b)
+igned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int i = 0;
+	unsigned int res = 0, base = 1;
+	int i;
 
-	if (b == NULL)
-		return (0);
-	if (b[i] == '\0')
+	if (b == '\0')
 		return (0);
 
-	while ((b[i] == '0') || (b[i] == '1'))
-	{
-		res <<= 1;
-		res += b[i] - '0';
+	for (i = 0; b[i];)
 		i++;
+
+	for (i -= 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		res += (b[i] - '0') * base;
+		base *= 2;
 	}
 
 	return (res);
